bcd6438f3caee71d63fe9d1d74a0b0ea10e57dae
inesdelgado0 <inesdelgado1400@gmail.com>
2026-03-16 01:28:16 +0000

    Setup inicial

---
+++ database-init/setup.sql
@@ -0,0 +1,89 @@
+CREATE TYPE estado_sessao AS ENUM ('Aberta', 'Esgotada', 'Finalizada', 'Cancelada');
+CREATE TYPE estado_bilhete AS ENUM ('Reservado', 'Pago', 'Cancelado');
+
+CREATE TABLE TipoUtilizador (
+    id SERIAL PRIMARY KEY,
+    tipo VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Sala (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    capacidade_Total INTEGER NOT NULL
+);
+
+CREATE TABLE Evento (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
+    descricao TEXT,
+    duracaoMin INTEGER,
+    classificacaoEtaria VARCHAR(20),
+    genero VARCHAR(50)
+);
+
+CREATE TABLE TipoBilhete (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    percentagemDesconto DECIMAL(5,2) DEFAULT 0
+);
+
+CREATE TABLE Pagamento (
+    id SERIAL PRIMARY KEY,
+    valorTotal DECIMAL(10,2) NOT NULL,
+    dataPagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    metodoPagamento VARCHAR(50)
+);
+
+
+CREATE TABLE Utilizador (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    telemovel VARCHAR(20),
+    morada TEXT,
+    nif VARCHAR(15),
+    id_tipo_utilizador INTEGER REFERENCES TipoUtilizador(id)
+);
+
+CREATE TABLE Zona (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    taxaAdicional DECIMAL(10,2) DEFAULT 0,
+    id_sala INTEGER REFERENCES Sala(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Lugar (
+    id SERIAL PRIMARY KEY,
+    fila VARCHAR(5) NOT NULL,
+    numero INTEGER NOT NULL,
+    id_zona INTEGER REFERENCES Zona(id) ON DELETE CASCADE,
+    UNIQUE (fila, numero, id_zona)
+);
+
+CREATE TABLE Sessao (
+    id SERIAL PRIMARY KEY,
+    dataHora TIMESTAMP NOT NULL,
+    precoBase DECIMAL(10,2) NOT NULL,
+    estado estado_sessao DEFAULT 'Aberta',
+    id_evento INTEGER REFERENCES Evento(id),
+    id_sala INTEGER REFERENCES Sala(id)
+);
+
+CREATE TABLE Bilhete (
+    id SERIAL PRIMARY KEY,
+    precoFinal DECIMAL(10,2) DEFAULT 0,
+    estado estado_bilhete DEFAULT 'Reservado',
+    id_utilizador INTEGER REFERENCES Utilizador(id),
+    id_sessao INTEGER REFERENCES Sessao(id),
+    id_pagamento INTEGER REFERENCES Pagamento(id)
+);
+
+CREATE TABLE Lugar_Bilhete (
+    id SERIAL PRIMARY KEY,
+    precoUnitario DECIMAL(10,2) DEFAULT 0,
+    id_bilhete INTEGER REFERENCES Bilhete(id) ON DELETE CASCADE,
+    id_lugar INTEGER REFERENCES Lugar(id),
+    id_tipo_bilhete INTEGER REFERENCES TipoBilhete(id),
+    UNIQUE (id_lugar, id_bilhete)
+);
